577108c66a2f6a0b3a6e756263ad523bedb79187
Shen, Xiao <Xiao.Shen@capco.com>
2018-06-29 09:12:17 -0400

    First commit

---
+++ src/test/java/com/xiao/tron/TronApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiao.tron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
